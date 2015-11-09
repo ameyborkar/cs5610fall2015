0829ea40a1485ef6fb7ff6b5081fe52832737485
Amey <ameyb@ccs.neu.edu>
2015-11-08 19:34:11 -0500

    project poc

---
+++ public/project/js/app.js
@@ -0,0 +1,39 @@
+﻿var app = angular.module("app", ["ngRoute"]);
+
+//all the routes
+app.config(function ($routeProvider) {
+    $routeProvider
+     
+    .when('/home', {
+        templateUrl: 'views/BookList.html',
+        controller: ''
+    })
+    .when('/login', {
+        templateUrl: 'views/Login.html',
+        controller: ''
+    })
+    .when('/register', {
+        templateUrl: 'views/Registration.html',
+        controller: ''
+    })
+    .when('/profile', {
+        templateUrl: 'views/Profile.html',
+        controller: '',
+        
+    })
+    .when('/book', {
+        templateUrl: 'views/Book.html',
+        controller: ''
+    })
+    .otherwise({
+        redirectTo: '/home'
+    })
+});
+
+
+
+
+
+
+
+
