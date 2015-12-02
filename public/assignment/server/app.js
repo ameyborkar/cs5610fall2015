9edc0bb9e27140c9f06572b1e144d9d43ee18d48
Amey <ameyb@ccs.neu.edu>
2015-12-02 00:03:28 -0500

    ougig

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -2,10 +2,11 @@
 
 module.exports = function(app, mongoose, db) {
 
-    var userModel = require("./models/user.model.js")(db, mongoose);
-    require("./services/user.service.server.js")(app, userModel);
-
-    var formModel = require('./models/form.model.js')(db, mongoose);
-    require('./services/form.service.js')(app, formModel);
-    require('./services/field.service.js')(app, formModel);
+	var userModel = require('./models/user.model.js')(db, mongoose);
+	require('./services/user.service.js')(app, userModel);
+	
+	var formModel = require('./models/form.model.js')(db, mongoose);
+	require('./services/form.service.js')(app, formModel);
+	require('./services/field.service.js')(app, formModel);
 };
+
